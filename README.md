cf30598513fe8b7e87626b20e0fa795211f7c82a
47seconds <vaibhavlakshkar.12345@gmail.com>
2023-09-24 16:53:24 +0530

    #

--- README.md
+++ README.md
@@ -1,35 +1,10 @@
-              /$$$$$$  /$$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$$
-            /$$__  $$| $$__  $$ /$$__  $$ /$$__  $$| $$_____/
-           | $$  \__/| $$  \ $$| $$  \ $$| $$  \__/| $$
-           |  $$$$$$ | $$$$$$$/| $$$$$$$$| $$      | $$$$$
-            \____  $$| $$____/ | $$__  $$| $$      | $$__/
-            /$$  \ $$| $$      | $$  | $$| $$    $$| $$
-           |  $$$$$$/| $$      | $$  | $$|  $$$$$$/| $$$$$$$$
-            \______/ |__/      |__/  |__/ \______/ |________/ 
-
-
-
-   /$$$$$$ /$$   /$$ /$$    /$$  /$$$$$$  /$$$$$$$  /$$$$$$$$ /$$$$$$$   /$$$$$$
-  |_  $$_/| $$$ | $$| $$   | $$ /$$__  $$| $$__  $$| $$_____/| $$__  $$ /$$__  $$ 
-    | $$  | $$$$| $$| $$   | $$| $$  \ $$| $$  \ $$| $$      | $$  \ $$| $$  \__/ 
-    | $$  | $$ $$ $$|  $$ / $$/| $$$$$$$$| $$  | $$| $$$$$   | $$$$$$$/|  $$$$$$ 
-    | $$  | $$  $$$$ \  $$ $$/ | $$__  $$| $$  | $$| $$__/   | $$__  $$ \____  $$ 
-    | $$  | $$\  $$$  \  $$$/  | $$  | $$| $$  | $$| $$      | $$  \ $$ /$$  \ $$ 
-   /$$$$$$| $$ \  $$   \  $/   | $$  | $$| $$$$$$$/| $$$$$$$$| $$  | $$|  $$$$$$/ 
-   |______/|__/  \__/    \_/    |__/  |__/|_______/ |________/|__/  |__/ \______/ 
-   
-  <hr>
-   
-
   ## SPACE INVADERS <hr>
 
   This is a pygame based game made and to be executed in Python 3.
 
   # This project does not accept pull requests.
-  <hr>
-    
+
   ## Usage
-  <hr>
 
   You need to install python3 version 3.1 and above to run this game
 
@@ -37,22 +12,6 @@
 
   you can change game BGM by putting music .wav file in sounds and changing music file address to new one in main.py code line 90.
    
-  Install pygame library using: "pip install pygame" in windows/MacOS/Linux cmd/terminal.<hr>
+  Install pygame library using: "pip install pygame" in windows/MacOS/Linux cmd/terminal.
     
   ## Build and maintained by: #47seconds
-
-  
-    .S_SsS_S.    .S  S.        sSSs          sSSs   .S_sSSs            sSSs   .S       S.    .S_sSSs    sdSS_SSSSSSbs   .S    sSSs 
-   .SS~S*S~SS.  .SS  SS.      d%%SP         d%%SP  .SS~YS%%b          d%%SP  .SS       SS.  .SS~YS%%b   YSSS~S%SSSSSP  .SS   d%%SP 
-   S%S `Y' S%S  S%S  S%S     d%S'          d%S'    S%S   `S%b        d%S'    S%S       S%S  S%S   `S%b       S%S       S%S  d%S' 
-   S%S     S%S  S%S  S%S     S%S           S%S     S%S    S%S        S%S     S%S       S%S  S%S    S%S       S%S       S%S  S%| 
-   S%S     S%S  S&S  S&S     S&S           S&S     S%S    d*S        S&S     S&S       S&S  S%S    d*S       S&S       S&S  S&S 
-   S&S     S&S  S&S  S&S     S&S_Ss        S&S_Ss  S&S   .S*S        S&S     S&S       S&S  S&S   .S*S       S&S       S&S  Y&Ss 
-   S&S     S&S  S&S  S&S     S&S~SP        S&S~SP  S&S_sdSSS         S&S     S&S       S&S  S&S_sdSSS        S&S       S&S  `S&&S 
-   S&S     S&S  S&S  S&S     S&S           S&S     S&S~YSY%b         S&S     S&S       S&S  S&S~YSY%b        S&S       S&S    `S*S 
-   S*S     S*S  S*S  S*b     S*b           S*b     S*S   `S%b        S*b     S*b       d*S  S*S   `S%b       S*S       S*S     l*S 
-   S*S     S*S  S*S  S*S.    S*S.          S*S.    S*S    S%S        S*S.    S*S.     .S*S  S*S    S%S       S*S       S*S    .S*P 
-   S*S     S*S  S*S   SSSbs   SSSbs         SSSbs  S*S    S&S         SSSbs   SSSbs_sdSSS   S*S    S&S       S*S       S*S  sSS*S 
-   SSS     S*S  S*S    YSSP    YSSP          YSSP  S*S    SSS          YSSP    YSSP~YSSY    S*S    SSS       S*S       S*S  YSS' 
-           SP   SP                                 SP                                       SP               SP        SP 
-           Y    Y                                  Y                                        Y                Y         Y
--- main.py
+++ main.py
@@ -1,3 +1,60 @@
+'''
+
+              /$$$$$$  /$$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$$
+            /$$__  $$| $$__  $$ /$$__  $$ /$$__  $$| $$_____/
+           | $$  \__/| $$  \ $$| $$  \ $$| $$  \__/| $$
+           |  $$$$$$ | $$$$$$$/| $$$$$$$$| $$      | $$$$$
+            \____  $$| $$____/ | $$__  $$| $$      | $$__/
+            /$$  \ $$| $$      | $$  | $$| $$    $$| $$
+           |  $$$$$$/| $$      | $$  | $$|  $$$$$$/| $$$$$$$$
+            \______/ |__/      |__/  |__/ \______/ |________/ 
+
+
+
+   /$$$$$$ /$$   /$$ /$$    /$$  /$$$$$$  /$$$$$$$  /$$$$$$$$ /$$$$$$$   /$$$$$$
+  |_  $$_/| $$$ | $$| $$   | $$ /$$__  $$| $$__  $$| $$_____/| $$__  $$ /$$__  $$ 
+    | $$  | $$$$| $$| $$   | $$| $$  \ $$| $$  \ $$| $$      | $$  \ $$| $$  \__/ 
+    | $$  | $$ $$ $$|  $$ / $$/| $$$$$$$$| $$  | $$| $$$$$   | $$$$$$$/|  $$$$$$ 
+    | $$  | $$  $$$$ \  $$ $$/ | $$__  $$| $$  | $$| $$__/   | $$__  $$ \____  $$ 
+    | $$  | $$\  $$$  \  $$$/  | $$  | $$| $$  | $$| $$      | $$  \ $$ /$$  \ $$ 
+   /$$$$$$| $$ \  $$   \  $/   | $$  | $$| $$$$$$$/| $$$$$$$$| $$  | $$|  $$$$$$/ 
+   |______/|__/  \__/    \_/    |__/  |__/|_______/ |________/|__/  |__/ \______/ 
+   
+   
+   ## SPACE INVADERS
+
+  This is a pygame based game made and to be executed in Python 3.
+
+  # This project does not accept pull requests.
+
+  ## Usage
+
+  You need to install python3 version 3.1 and above to run this game
+
+  To run game, open terminal in SpaceInvader folder and type: "python .\main.py" to run. For MacOS and other exception, use: "python3 .\main.py" or "python3 ./main.py"
+
+  you can change game BGM by putting music .wav file in sounds and changing music file address to new one in main.py code line 90.
+   
+  Install pygame library using: "pip install pygame" in windows/MacOS/Linux cmd/terminal.
+    
+  ## Build and maintained by:
+  
+                                 .x+=:.                                                     ..          .x+=:.   
+        xeee    dL ud8Nu  :8c   z`    ^%                                                  dF           z`    ^%  
+       d888R    8Fd888888L %8      .   <k                             u.      u.    u.   '88bu.           .   <k 
+      d8888R    4N88888888cuR    .@8Ned8"      .u          .    ...ue888b   x@88k u@88c. '*88888bu      .@8Ned8" 
+     @ 8888R    4F   ^""%""d   .@^%8888"    ud8888.   .udR88N   888R Y888r ^"8888""8888"   ^"*8888N   .@^%8888"  
+   .P  8888R    d       .z8   x88:  `)8b. :888'8888. <888'888k  888R I888>   8888  888R   beWE "888L x88:  `)8b. 
+  :F   8888R    ^     z888    8888N=*8888 d888 '88%" 9888 'Y"   888R I888>   8888  888R   888E  888E 8888N=*8888 
+ x"    8888R        d8888'     %8"    R88 8888.+"    9888       888R I888>   8888  888R   888E  888E  %8"    R88 
+d8eeeee88888eer    888888       @8Wou 9%  8888L      9888      u8888cJ888    8888  888R   888E  888F   @8Wou 9%  
+       8888R      :888888     .888888P`   '8888c. .+ ?8888u../  "*888*P"    "*88*" 8888" .888N..888  .888888P`   
+       8888R       888888     `   ^"F      "88888%    "8888P'     'Y"         ""   'Y"    `"888*""   `   ^"F     
+    "*%%%%%%**~    '%**%                     "YP'       "P'                                  ""                  
+   
+   
+   '''
+
 import pygame
 import random
 import math
@@ -214,3 +271,24 @@ while running:
 
     # Update game screen
     pygame.display.update()
+    
+    
+    
+    '''
+    
+    .S_SsS_S.    .S  S.        sSSs          sSSs   .S_sSSs            sSSs   .S       S.    .S_sSSs    sdSS_SSSSSSbs   .S    sSSs 
+   .SS~S*S~SS.  .SS  SS.      d%%SP         d%%SP  .SS~YS%%b          d%%SP  .SS       SS.  .SS~YS%%b   YSSS~S%SSSSSP  .SS   d%%SP 
+   S%S `Y' S%S  S%S  S%S     d%S'          d%S'    S%S   `S%b        d%S'    S%S       S%S  S%S   `S%b       S%S       S%S  d%S' 
+   S%S     S%S  S%S  S%S     S%S           S%S     S%S    S%S        S%S     S%S       S%S  S%S    S%S       S%S       S%S  S%| 
+   S%S     S%S  S&S  S&S     S&S           S&S     S%S    d*S        S&S     S&S       S&S  S%S    d*S       S&S       S&S  S&S 
+   S&S     S&S  S&S  S&S     S&S_Ss        S&S_Ss  S&S   .S*S        S&S     S&S       S&S  S&S   .S*S       S&S       S&S  Y&Ss 
+   S&S     S&S  S&S  S&S     S&S~SP        S&S~SP  S&S_sdSSS         S&S     S&S       S&S  S&S_sdSSS        S&S       S&S  `S&&S 
+   S&S     S&S  S&S  S&S     S&S           S&S     S&S~YSY%b         S&S     S&S       S&S  S&S~YSY%b        S&S       S&S    `S*S 
+   S*S     S*S  S*S  S*b     S*b           S*b     S*S   `S%b        S*b     S*b       d*S  S*S   `S%b       S*S       S*S     l*S 
+   S*S     S*S  S*S  S*S.    S*S.          S*S.    S*S    S%S        S*S.    S*S.     .S*S  S*S    S%S       S*S       S*S    .S*P 
+   S*S     S*S  S*S   SSSbs   SSSbs         SSSbs  S*S    S&S         SSSbs   SSSbs_sdSSS   S*S    S&S       S*S       S*S  sSS*S 
+   SSS     S*S  S*S    YSSP    YSSP          YSSP  S*S    SSS          YSSP    YSSP~YSSY    S*S    SSS       S*S       S*S  YSS' 
+           SP   SP                                 SP                                       SP               SP        SP 
+           Y    Y                                  Y                                        Y                Y         Y
+           
+   '''
